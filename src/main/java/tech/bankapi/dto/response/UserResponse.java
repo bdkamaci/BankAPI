3d728622512a48287ca5aa6838f9385c3b4c8c72
bdkamaci <burcudogakamaci@gmail.com>
2024-09-24 14:45:58 +0200

    refactor: add token field to UserResponse for JWT support

--- src/main/java/tech/bankapi/dto/response/UserResponse.java
+++ src/main/java/tech/bankapi/dto/response/UserResponse.java
@@ -14,6 +14,7 @@ public class UserResponse {
     private String name;
     private String email;
     private String phone;
+    private String token;
 
     private List<AccountResponse> accounts;
     private List<SecurityLogResponse> securityLogs;
