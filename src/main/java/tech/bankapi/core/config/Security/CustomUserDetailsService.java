a3f6da12fe5d5598470ae7b6a29a1d2ed3af7a2c
bdkamaci <burcudogakamaci@gmail.com>
2024-11-03 20:59:07 +0100

    fix: fix bugs for deployment

---
+++ src/main/java/tech/bankapi/core/config/Security/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package tech.bankapi.core.config.Security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.bankapi.model.User;
+import tech.bankapi.repository.UserRepository;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                true, // account is not expired
+                true, // credentials are not expired
+                true, // account is not locked
+                true, // account is enabled
+                new ArrayList<>() // no authorities
+        );
+    }
+}
--- src/main/java/tech/bankapi/security/JwtRequestFilter.java
+++ src/main/java/tech/bankapi/security/JwtRequestFilter.java
@@ -52,7 +52,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
             // Validate token considering the lastLogoutTime
             if (jwtUtil.validateToken(jwt, userDetails, lastLogoutTime)) {
                 UsernamePasswordAuthenticationToken authenticationToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                        new UsernamePasswordAuthenticationToken(userDetails, null, null);
                 authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             }
